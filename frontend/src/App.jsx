2bd68daecf1e826c8a5d4adea6459d5dc5c5277a
Avinash <avinashnayak573@gmail.com>
2024-02-17 21:39:19 +0530

    Add @anon-aadhaar/react dependency and update App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import { AnonAadhaarProvider } from "@anon-aadhaar/react";
+import Home from './Home';
 
-function App() {
+export default function App() {
+  const [ready, setReady] = useState(false);
+  const [useTestAadhaar, setUseTestAadhaar] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      {ready && (
+        <AnonAadhaarProvider _useTestAadhaar={useTestAadhaar}>
+          <Home setUseTestAadhaar={setUseTestAadhaar} useTestAadhaar={useTestAadhaar} />
+        </AnonAadhaarProvider>
+      )}
+    </>
+  );
 }
-
-export default App
